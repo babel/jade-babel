1b9f3c7bd4bd1ee1f84fc30f48b3f795546bc975
Apox <xzzy159@gmail.com>
2014-09-13 11:48:08 +0200

    Update to Traceur 0.0.62 and add test

---
+++ test/test.js
@@ -0,0 +1,19 @@
+require('chai').should()
+
+var jade = require('jade')
+var jadeTraceur = require('..')
+
+describe('compile', function(){			
+	var expected =	'<html><header></header><body><script>"use strict";\n' +
+				  	'var Test = function Test(test) {\n' +
+				  	'  this.test = test;\n' +
+					'};\n' +
+					'($traceurRuntime.createClass)(Test, {}, {});\n' +
+					'</script></body></html>'
+	it('should be the right compiled output', function(){
+		var jadeT = jadeTraceur({}, jade)
+		var compiled = jadeT.compileFile('./test/jade/test.jade')
+		var result = compiled()
+		result.should.be.equal(expected)
+	})
+})
