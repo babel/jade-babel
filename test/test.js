6e46333966e63a8d7eee52905d0fe01f4bdaa2a2
Sebastian McKenzie <sebmck@gmail.com>
2015-04-03 06:08:11 +1100

    Fix test

--- test/test.js
+++ test/test.js
@@ -4,9 +4,9 @@ var jade = require('jade');
 var jadeTraceur = require('..');
 
 describe('compile', function(){
-	var expected ='<html><header></header><body><script>\"use strict\";\n\nvar Test = function Test(test) {\n  this.test = test;\n};</script></body></html>';
+	var expected ='<html><header></header><body><script>\"use strict\";\n\nvar Test = function Test(test) {\n  babelHelpers.classCallCheck(this, Test);\n\n  this.test = test;\n};</script></body></html>';
 	it('should be the right compiled output', function(){
-		var jadeT = jadeTraceur({}, jade);
+		var jadeT = jadeTraceur({ externalHelpers: true }, jade);
 		var compiled = jadeT.compileFile('./test/jade/test.jade');
 		var result = compiled();
 		result.should.be.equal(expected);
